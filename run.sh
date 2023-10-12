8ebb78d2af902cc9fbf7a29305495791530c7750
Steven Young <steven.young114@gmail.com>
2023-10-12 21:55:45 +1100

    fix argument position bug

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 #if I want to compile from the beginning...
 if [ "$1" = "--reset" ]; then
-    rm -rf build; mkdir build; cd build; cmake ..; make all; ./geoviz $1; cd ..
+    rm -rf build; mkdir build; cd build; cmake ..; make all; ./geoviz $2; cd ..
 
 #debug with gdb. need to recompile everything here
 elif [ "$1" = "--debug" ]; then
-    rm -rf build; mkdir build; cd build; cmake -DCMAKE_BUILD_TYPE=Debug ..; make all; gdb geoviz $1; cd ..
+    rm -rf build; mkdir build; cd build; cmake -DCMAKE_BUILD_TYPE=Debug ..; make all; gdb geoviz $2; cd ..
 
 #run normally without compiling dependences (assuming they are already compiled)
 else
