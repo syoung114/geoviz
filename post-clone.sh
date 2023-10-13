e8bec47a4bdef91229f65a767c0ea5bb9f6d7692
Steven Young <steven.young114@gmail.com>
2023-10-13 16:32:32 +1100

    remove post-clone.sh

--- post-clone.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#go to https://glad.dav1d.de/ to generate the files and copy the link for glad.zip. That is what $1 is.
-git submodule update --init --recursive; cd lib/glad/; curl $1 -o "glad.zip"; unzip glad.zip; rm glad.zip; cd ../../
